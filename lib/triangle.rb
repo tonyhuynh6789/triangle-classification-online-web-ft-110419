67b3db68d1ecbb379b70d6c813555edbf02c0522
Tony Rim <duc_thinh_03@yahoo.com>
2019-12-08 22:33:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
-class Triangle
-  # write code here
-end
+class Triangle 
+
+  def initialize(size_one, size_two, size_three)
+    @size_one = size_one
+    @size_two = size_two
+    @size_three = size_three
+  end 
+
+
+  def kind
+    if (@size_one <= 0) || (@size_two <= 0) || (@size_three <= 0)
+      raise TriangleError
+    elsif (@size_one + @size_two <= @size_three) || (@size_two + @size_three <= @size_one) || (@size_one + @size_three <= @size_two)
+      raise TriangleError
+    else 
+      if (@size_one == @size_two) && (@size_two == @size_three)
+        :equilateral 
+      elsif (@size_one == @size_two) || (@size_two == @size_three) || (@size_one == @size_three)
+        :isosceles 
+      elsif (@size_one != @size_two) && (@size_two != @size_three) && (@size_one != @size_three)
+          :scalene
+      end 
+    end 
+  end 
+       
+  class TriangleError < StandardError 
+
+  end 
+end 
